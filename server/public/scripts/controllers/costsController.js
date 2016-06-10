c1148184d930d148abc3c4a2aff569f7c25f2008
Hannah <okami118@Hannah-Springers-MacBook-Pro.local>
2016-06-10 17:03:00 -0500

    Cost page base functonality complete; items can be added to each category,  total cost displayed at bottom of page, delete button present for each item

--- server/public/scripts/controllers/costsController.js
+++ server/public/scripts/controllers/costsController.js
@@ -2,9 +2,15 @@ myApp.controller('CostsController', ['$scope', '$http', '$window', '$location', 
 $scope.dataFactory = DataFactory;
 $scope.factoryCurrentTrip = $scope.dataFactory.factoryCurrentTrip;
 $scope.currentCostCategory = {};
+$scope.displayedCategoryID = '';
+$scope.newExpense = {};
 
   console.log($scope.factoryCurrentTrip.data.costs);
 
+  $scope.addItem = function (id) {
+    console.log('showAddItem', id);
+    $scope.displayedCategoryID = id;
+  };
 
     $scope.submitNewCategory = function () {
       console.log($scope.currentCostCategory);
@@ -15,14 +21,36 @@ $scope.currentCostCategory = {};
       });
   };
 
-    $scope.addNewItem = function (category) {
-      var categoryID = category._id;
-        $http.put('/selectedTrip/' + categoryID, category)
+    $scope.submitNewItem = function (id) {
+      var item = $scope.newExpense;
+        $http.put('/selectedTrip/' + $scope.factoryCurrentTrip.data._id + '/category/' + id + '/items', item)
           .then(function (response) {
-            console.log('PUT /selectedTrip ', category);
+            console.log('PUT /selectedTrip ', item);
             loadUpdatedTrip();
           });
       };
+
+  $scope.deleteItem = function (id1, id2) {
+  $http.delete('/selectedTrip/' + $scope.factoryCurrentTrip.data._id + '/category/' + id1 + '/item/' + id2)
+    .then(function (response) {
+      console.log('DELETE /item ', id2);
+      loadUpdatedTrip();
+      });
+   };
+
+   $scope.getTotal = function(){
+    var total = 0;
+    for(var i = 0; i < $scope.factoryCurrentTrip.data.costs.length; i++){
+        var category = $scope.factoryCurrentTrip.data.costs[i];
+        for(var j = 0; j < category.items.length; j++) {
+        var item = category.items[j].cost;
+        console.log(item);
+        total += (item);
+        }
+    }
+    console.log(total);
+    return total;
+};
 
   function loadUpdatedTrip() {
       $scope.dataFactory.factoryGetSelectedTrip($scope.factoryCurrentTrip.data._id);
--- server/routes/selectedTrip.js
+++ server/routes/selectedTrip.js
@@ -31,4 +31,52 @@ router.put('/:id', function (req, res) {
   });
 });
 
+router.put('/:_id/category/:id/items', function (req, res) {
+  console.log(req.body);
+  console.log(req.params);
+  Trip.findById(req.params._id, function (err, trip) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+
+    var doc = trip.costs.id(req.params.id);
+    console.log(doc);
+    doc.items.push(req.body);
+
+    trip.save(function (err) {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+      }
+
+      res.sendStatus(204);
+    });
+  });
+});
+
+router.delete('/:_id/category/:id1/item/:id2', function (req, res) {
+    Trip.findById(req.params._id, function (err, trip) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+
+    var doc = trip.costs.id(req.params.id1).items.id(req.params.id2).remove();
+    console.log(doc);
+
+    trip.save(function (err) {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+      }
+
+    res.sendStatus(204);
+  });
+  });
+});
+
 module.exports = router;
